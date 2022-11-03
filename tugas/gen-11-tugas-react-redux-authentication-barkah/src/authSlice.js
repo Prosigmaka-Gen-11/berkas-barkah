f3b7067daaaaf0c808f3199b697c9489ae44a786
nbarkah <nurhaibarkah9@gmail.com>
2022-11-03 17:16:43 +0700

    update tugas react redux auth

--- tugas/gen-11-tugas-react-redux-authentication-barkah/src/authSlice.js
+++ tugas/gen-11-tugas-react-redux-authentication-barkah/src/authSlice.js
@@ -1,6 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-// blm kepake pas refresh malah jadi undefined (pas refresh jadi error putih semua)
 function getLocalUserData() {
   const savedUserData = localStorage.getItem("userData");
 
@@ -12,14 +11,13 @@ function getLocalUserData() {
   }
 }
 
-// ini bisa terus pas refresh juga udah
 function getLocalToken() {
   const savedToken = localStorage.getItem("token");
   return savedToken ?? null;
 }
 
 const initialState = {
-  userData: {},
+  userData: getLocalUserData(),
   token: getLocalToken(),
   isLogin: getLocalToken() != null,
 };
@@ -31,12 +29,12 @@ const authSlice = createSlice({
     handlerAfterLogin(state, action) {
       state.userData = action.payload;
       state.token = action.payload.token;
-      localStorage.setItem("userData", JSON.stringify(action.payload.userData));
+      localStorage.setItem("userData", JSON.stringify(action.payload));
       localStorage.setItem("token", action.payload.token);
       state.isLogin = action.payload.token ? action.payload.token : null;
     },
 
-    logout(state, action) {
+    logout(state) {
       localStorage.removeItem("userData");
       localStorage.removeItem("token");
       state.userData = {};
